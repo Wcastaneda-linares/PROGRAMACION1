8cfc615d55f983bc830e509466db218ad3e56895
Wcastaneda-linares <wcastanedal@gmail.com>
2021-02-19 14:58:19 -0600

    PROGRAMACION1
    TAREA2

---
+++ 2. DESCUENTO/EJERCICIO2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DESCUENTO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal compra;
+            decimal descuento;
+            decimal total;
+            Console.Write("INGRESE SU TOTAL DE COMPRA: ");
+            compra = decimal.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine();
+            if (compra > 300)
+            {
+                descuento = (compra * 20) / 100;
+                Console.WriteLine("SU DESCUENTO ES DE:     " + descuento);
+                total = compra - descuento;
+                Console.WriteLine("SU TOTAL DE PAGO ES DE: " + total);
+            }
+            else
+            {
+                Console.WriteLine("USTED NO TIENE DESCUENTO :( ");
+                Console.WriteLine("SU TOTAL A PAGAR ES DE: " + compra);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
